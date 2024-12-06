12127ed9c195ada4f20531b6c78a0d12db74fef9
elbitoku <leo.langlois123@gmail.com>
2024-12-07 00:01:59 +0100

    404

--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -1,6 +1,23 @@
+import Navbar from "../components/Navbar/Navbar";
+import { useNavigate } from "react-router-dom";
+
 function NoPage() {
+  const navigate = useNavigate();
+
+  const handleGoHome = () => {
+    navigate('/')
+  }
+
   return (
-      <h1>Je suis la nouvelle page NoPage</h1>
+      <>
+      <Navbar/>
+      <div className="notfound">
+        <p style={{color: "grey"}}>404</p>
+        <h1>Page non trouvé</h1>
+        <p style={{color: "grey"}}>Désolé, nous n'arrivons pas à trouver la page que vous cherchez.</p>
+        <button onClick={handleGoHome}>Retour à l'Accueil</button>
+      </div>
+      </>
   );
 };
 
