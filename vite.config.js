26995eaeb32a03ff45d3e02d1a6e80dcb8de106e
elbitoku <leo.langlois123@gmail.com>
2024-12-08 23:11:30 +0100

    mobile

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Portfolio/', 
   plugins: [react()],
+  server: {
+    hmr: true,
+  },
+  build: {
+    outDir: "dist",
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  base: "/",
 });
